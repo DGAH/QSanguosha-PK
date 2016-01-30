6fb6cb6206fa3e01760f44ae673d61d42ec1e02a
DGAH <1346952149@qq.com>
2016-01-30 15:26:36 +0800

    整理台词和配音

---
+++ lang/zh_CN/Audio/TestPackageLines.lua
@@ -0,0 +1,49 @@
+-- lines for Test Package
+
+return {
+	-- 制霸孙权
+	["$super_zhiheng1"] = "容我三思。",
+	["$super_zhiheng2"] = "且慢。",
+	["~zhiba_sunquan"] = "父亲，大哥，仲谋愧矣……",
+	-- 四庸袁术
+	["cv:super_yuanshu"] = "名将三国",
+	["$super_yongsi1"] = "嘿呀，还不错~",
+	["$super_yongsi2"] = "呐~哈哈哈哈哈哈哈哈哈哈！",
+	["$super_yongsi3"] = "呀呀呀呀呀呀呀！",
+	["$super_yongsi4"] = "嗙啪~呜哈哈哈哈哈哈哈哈！",
+	["$super_weidi1"] = "我才是皇帝！",
+	["$super_weidi2"] = "你们都得听我的号令！",
+	["~super_yuanshu"] = "呃啊~~~~~~~~",
+	-- 蹲坑曹仁
+	["$super_jushou1"] = "我先休息一会儿！",
+	["$super_jushou2"] = "尽管来吧！",
+	["~super_caoren"] = "实在是守不住了……",
+	-- 无崩董卓
+	["cv:nobenghuai_dongzhuo"] = "やまもとみ，迷宫女友(肉林2)",
+	["$jiuchi"] = "醉酒销魂啊~",
+	["$roulin1"] = "屈我淫威，哈哈~",
+	["$roulin2"] = "董贼，拿命来！",
+	["$roulin3"] = "去死吧变态！", -- 女性酒杀董卓时播放
+	["$baonue"] = "天下群雄，唯我独尊！",
+	["$benghuai1"] = "不朽之躯，天却亡我……", -- 男
+	["$benghuai2"] = "不，不可以的……", -- 女
+	["~nobenghuai_dongzhuo"] = "吾儿奉先何在？呃...",
+	-- 最强神话
+	["cv:zqsh"] = "风叹息",
+	["$wushuang1"] = "三个齐上也不是我的对手！",
+	["$wushuang2"] = "还有哪个敢挑战我？",
+	["$wushuang3"] = "谁能挡我！",
+	["$wushuang4"] = "神挡杀神，佛挡杀佛！",
+	["~zqsh"] = "杂鱼们，好戏才刚刚开始！",
+	-- 暴怒战神
+	["cv:bnzs"] = "风叹息",
+	["$xiuluo"] = "哼！鬼蜮伎俩，休想阻我！",
+	["$shenwei"] = "飞将之威，上天亦知！",
+	["$shenji"] = "神戟在手，何人能及！",
+	["~bnzs"] = "什么？我败了？！",
+	-- 僵尸
+	["cv:zombie"] = "不详",
+	["$paoxiao"] = "啊~~~",
+	["$wansha"] = "汝今势孤，命必绝矣！",
+	["~zombie"] = "已死过一次，再死一次又有何妨？",
+}
