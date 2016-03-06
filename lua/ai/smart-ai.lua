5557e12b0ceece2d1b20c4eab65514f9febd139a
DGAH <1346952149@qq.com>
2016-03-06 12:38:03 +0800

    开放旧式AI扩展功能

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -1,3 +1,6 @@
+--[[
+	太阳神三国杀武将单挑对战平台·AI核心文件
+]]--
 -- "middleclass" is the Lua OOP library written by kikito
 -- more information see: https://github.com/kikito/middleclass
 require "middleclass"
@@ -13,7 +16,7 @@ version = "QSanguosha-PK Dummy AI"
 function CloneAI(player)
 	return SmartAI(player).lua_ai
 end
-sgs.ais = {}
+dofile "lua/ai/tables.lua"
 function SmartAI:initialize(player)
 	self.player = player
 	self.room = player:getRoom()
@@ -63,16 +66,58 @@ function SmartAI:filterEvent(event, player, data)
 	end
 end
 function SmartAI:askForSuit(reason)
+	local callback = sgs.askForSuitAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self)
+	elseif type(callback) == "number" then
+		result = callback
+	end
+	if type(result) == "number" then
+		if result >= 0 and result <= 3 then
+			return result
+		end
+	end
 	return math.random(0, 3)
 end
 function SmartAI:askForSkillInvoke(skill_name, data)
+	local callback = sgs.askForSkillInvokeAI[skill_name]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, data)
+	elseif type(callback) == "boolean" then
+		result = callback
+	end
+	if type(result) == "boolean" then
+		return result
+	end
 	return math.random(0, 100) < 50
 end
 function SmartAI:askForChoice(skill_name, choices, data)
+	local callback = sgs.askForChoiceAI[skill_name]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, choices, data)
+	elseif type(callback) == "string" then
+		result = callback
+	end
+	if type(callback) == "string" then
+		if string.find(choices, result) then
+			return result
+		end
+	end
 	local items = choices:split("+")
 	return items[math.random(1, #items)]
 end
 function SmartAI:askForDiscard(reason, discard_num, min_num, optional, include_equip)
+	local callback = sgs.askForDiscardAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, discard_num, min_num, optional, include_equip)
+	end
+	if type(result) == "table" then
+		return result
+	end
 	if optional then
 		return {}
 	end
@@ -96,8 +141,24 @@ function SmartAI:askForDiscard(reason, discard_num, min_num, optional, include_e
 	return to_discard
 end
 function SmartAI:askForNullification(trick, from, to, positive)
+	for _,callback in ipairs(sgs.askForNullificationAI) do
+		if type(callback) == "function" then
+			local result = callback(self, trick, from, to, positive)
+			if type(result) == "string" then
+				return result
+			end
+		end
+	end
 end
 function SmartAI:askForCardChosen(who, flags, reason, method)
+	local callback = sgs.askForCardChosenAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, who, flags, method)
+	end
+	if type(result) == "number" then
+		return result
+	end
 	local cards = who:getCards(flags)
 	if method == sgs.Card_MethodDiscard then
 		local can_discard = {}
@@ -112,6 +173,17 @@ function SmartAI:askForCardChosen(who, flags, reason, method)
 	end
 end
 function SmartAI:askForCard(pattern, prompt, data)
+	local promptlist = promptlist:split(":")
+	local reason, src, dest, arg, arg2 = promptlist[1], promptlist[2], promptlist[3], promptlist[4], promptlist[5]
+	reason = reason or "dummy"
+	local callback = sgs.askForCardAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, pattern, data, src, dest, arg, arg2)
+	end
+	if type(result) == "string" then
+		return result
+	end
 	local cards = self.player:getCards("he")
 	local can_response = {}
 	for _,c in sgs.qlist(cards) do
@@ -125,30 +197,95 @@ function SmartAI:askForCard(pattern, prompt, data)
 	return can_response[math.random(1, #can_response)]:getEffectiveId()
 end
 function SmartAI:askForUseCard(pattern, prompt, method)
+	local callback = sgs.askForUseCardAI[pattern]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, prompt, method)
+	end
+	if type(result) == "string" then
+		return result
+	end
 	return "."
 end
 function SmartAI:askForAG(card_ids, refusable, reason)
+	local callback = sgs.askForAGAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, card_ids, refusable)
+	end
+	if type(result) == "number" then
+		return number
+	end
 	if refusable then
 		return -1
 	end
 	return card_ids[math.random(1, #card_ids)]
 end
 function SmartAI:askForCardShow(requestor, reason)
+	local callback = sgs.askForCardShowAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, requestor)
+	end
+	if type(result) == "userdata" and result:inherits("Card") then
+		return result
+	end
 	return self.player:getRandomHandCard()
 end
 function SmartAI:askForYiji(card_ids, reason)
+	local callback = sgs.askForYijiAI[reason]
+	local target, id
+	if type(callback) == "function" then
+		target, id = callback(self, card_ids)
+	end
+	if type(id) == "number" then
+		return target, id
+	end
 	return nil, -1
 end
 function SmartAI:askForPindian(requestor, reason)
+	local callback = sgs.askForPindianAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, requestor)
+	end
+	if type(result) == "userdata" and result:inherits("Card") then
+		return result
+	end
 	return self.player:getRandomHandCard()
 end
 function SmartAI:askForPlayerChosen(targets, reason)
+	local callback = sgs.askForPlayerChosenAI[reason]
+	local result
+	if type(callback) == "function" then
+		result = callback(self, targets)
+	end
+	if type(result) == "userdata" and result:inherits("ServerPlayer") then
+		return result
+	end
 	return targets:at(math.random(0, targets:length()-1))
 end
 function SmartAI:askForSinglePeach(dying)
+	for _,callback in ipairs(sgs.askForSinglePeachAI) do
+		if type(callback) == "function" then
+			local result = callback(self, dying)
+			if type(result) == "string" then
+				return result
+			end
+		end
+	end
 	return "."
 end
 function SmartAI:askForGuanxing(cards, guanxing_type)
+	local reason = "dummy"
+	local callback = sgs.askForGuanxingAI[reason]
+	local up, down
+	if type(callback) == "function" then
+		up, down = callback(self, cards, guanxing_type)
+	end
+	if type(up) == "table" and type(down) == "table" then
+		return up, down
+	end
 	if guanxing_type == sgs.Room_GuanxingBothSides then
 		return cards, {}
 	elseif guanxing_type == sgs.Room_GuanxingUpOnly then
@@ -159,4 +296,12 @@ function SmartAI:askForGuanxing(cards, guanxing_type)
 	return cards, {}
 end
 function SmartAI:activate(use)
+	for _,callback in ipairs(sgs.activateAI) do
+		if type(callback) == "function" then
+			callback(self, use)
+			if use:isValid() then
+				return
+			end
+		end
+	end
 end
---
+++ lua/ai/tables.lua
@@ -0,0 +1,20 @@
+--[[
+	太阳神三国杀武将单挑对战平台·AI系统用表声明文件
+]]--
+sgs.ais = {}
+sgs.askForSuitAI = {}
+sgs.askForSkillInvokeAI = {}
+sgs.askForChoiceAI = {}
+sgs.askForDiscardAI = {}
+sgs.askForNullificationAI = {}
+sgs.askForCardChosenAI = {}
+sgs.askForCardAI = {}
+sgs.askForUseCardAI = {}
+sgs.askForAGAI = {}
+sgs.askForCardShowAI = {}
+sgs.askForYijiAI = {}
+sgs.askForPindianAI = {}
+sgs.askForPlayerChosenAI = {}
+sgs.askForSinglePeachAI = {}
+sgs.askForGuanxingAI = {}
+sgs.activateAI = {}
